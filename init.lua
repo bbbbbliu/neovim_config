889a2dfe285c99de491b005e6fab49050d367803
kinkypotato45 <u1175421@umail.utah.edu>
2024-05-13 12:42:44 -0600

    changed colorscheme, added lazying loading and snippets

---
+++ init.lua
@@ -0,0 +1,4 @@
+require "bliu.options"
+require "bliu.keymaps"
+require "bliu.plugins"
+vim.cmd "colorscheme dracula"
--- lua/bliu/plugins.lua
+++ lua/bliu/plugins.lua
@@ -18,7 +18,40 @@ local plugins = {
   "folke/lazy.nvim",
   "dracula/vim",
   "williamboman/mason.nvim",
-  "nvim-treesitter/nvim-treesitter"
+  -- Treesitter
+  "nvim-treesitter/nvim-treesitter",
+  {
+   "folke/which-key.nvim",
+   event = "VeryLazy",
+   init = function()
+     vim.o.timeout = true
+     vim.o.timeoutlen = 300
+   end,
+   opts = {
+     -- your configuration comes here
+     -- or leave it empty to use the default settings
+     -- refer to the configuration section below
+   }
+  },
 
-}
+  -- Cmp
+  "hrsh7th/nvim-cmp",  -- The completion plugin
+  "hrsh7th/cmp-nvim-lua", 
+  "hrsh7th/cmp-path", -- path completions
+	"saadparwaiz1/cmp_luasnip",  -- snippet completions
+	"hrsh7th/cmp-nvim-lsp", 
+	"hrsh7th/cmp-nvim-lua",
+  {'akinsho/bufferline.nvim', version = "*", dependencies = 'nvim-tree/nvim-web-devicons'},
+
+  -- snippets
+  {
+  	"L3MON4D3/LuaSnip",
+	  -- follow latest release.
+	  version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+  	-- install jsregexp (optional!).
+   	build = "make install_jsregexp",
+  dependencies = { "rafamadriz/friendly-snippets" }
+  }
+
+  }
 require("lazy").setup(plugins, opts)
